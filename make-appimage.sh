99b5dae68266a60c8ff969014cf9bc59eef8f920
Link4Electronics <link4electronics@gmail.com>
2026-09-17 18:09:17 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,22 +3,26 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/scalable/apps/com.cataclysm-tlg.cataclysm-tlg.svg
+export DESKTOP=/usr/share/applications/com.cataclysm-tlg.cataclysm-tlg.desktop
+export APPNAME=Cataclysm-TLG
+export STARTUPWMCLASS=cataclysm-tlg-tiles
+export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/cataclysm-tlg-tiles /usr/share/cataclysm-tlg
 
-# Additional changes can be done in between here
+# fontdata.json ships legacy "data/font/..." paths for repo-root layouts; the
+# game resolves fonts relative to datadir/font/, so strip the prefix
+sed -i 's#data/font/##g' AppDir/share/cataclysm-tlg/fontdata.json
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the test fails due to the app
 # having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
+quick-sharun --simple-test ./dist/*.AppImage
